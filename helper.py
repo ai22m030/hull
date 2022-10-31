71ac5449d150d09a744aa2c8a119cc6f116122e8
ai22m040 <ai22m040@technikum-wien.at>
2022-10-31 18:35:31 +0100

    added helper.py to generate import files use txxx.csv, rxxx.csv and pxxx.csv to create performance test cases please write result of test cases in runtime_analysis.txt

---
+++ helper.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+Helper program to generate csv Files for data import
+
+"""
+
+import matplotlib.pyplot as plt
+import random
+import pandas as pd
+
+
+# %% functions
+
+def points_in_triangle(pt1, pt2, pt3):
+    """
+    Random point in the triangle with vertices pt1, pt2 and pt3.
+    """
+    x, y = random.random(), random.random()
+    q = abs(x - y)
+    s, t, u = q, 0.5 * (x + y - q), 1 - 0.5 * (q + x + y)
+    return (
+        s * pt1[0] + t * pt2[0] + u * pt3[0],
+        s * pt1[1] + t * pt2[1] + u * pt3[1],
+    )
+
+def points_on_rectangle(pt1, pt2, pt3, pt4):
+    """
+    Random point on the rectangle with vertices pt1, pt2, pt3 and pt4.
+    """
+    
+    list = [pt1,pt2, pt3, pt4]
+    min_x = min(list, key=lambda p:p[0])[0]
+    max_x = max(list, key=lambda p:p[0])[0]
+    min_y = min(list, key=lambda p:p[1])[1]
+    max_y = max(list, key=lambda p:p[1])[1]
+    
+    line = random.randint(0,3)
+
+    if line == 0: # point on lower line
+        return (min_x + (max_x - min_x) * random.random(),min_y)
+    elif line == 1: # point on upper line
+        return (min_x + (max_x - min_x) * random.random(),max_y)
+    elif line == 2: # point on left line
+        return (min_x, min_y + (max_y - min_y) * random.random())
+    elif line == 3: # point on right line
+        return (max_x, min_y + (max_y - min_y) * random.random())
+
+def points_within_polygone(pt1, pt2, pt3, pt4):
+    """
+    Random point on the rectangle with vertices pt1, pt2, pt3 and pt4.
+    """
+    
+    list = [pt1,pt2, pt3, pt4]
+    min_x = min(list, key=lambda p:p[0])[0]
+    max_x = max(list, key=lambda p:p[0])[0]
+    min_y = min(list, key=lambda p:p[1])[1]
+    max_y = max(list, key=lambda p:p[1])[1]
+    
+    line = random.randint(0,3)
+
+    if line == 0: # point on lower line
+        return (min_x + (max_x - min_x) * random.random(), min_y + (max_y - min_y) * random.random())
+    elif line == 1: # point on upper line
+        return (min_x + (max_x - min_x) * random.random(), min_y + (max_y - min_y) * random.random())
+    elif line == 2: # point on left line
+        return (min_x + (max_x - min_x) * random.random(), min_y + (max_y - min_y) * random.random())
+    elif line == 3: # point on right line
+        return (min_x + (max_x - min_x) * random.random(), min_y + (max_y - min_y) * random.random())
+
+# %% triangle 100 points within
+
+# triangle with 100 points
+random.seed()
+
+pt1 = (50, 50)
+pt2 = (1000, 80)
+pt3 = (300, 770)
+points = [points_in_triangle(pt1, pt2, pt3) for _ in range(97)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0]],
+                    "y":[pt1[1], pt2[1], pt3[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("t100.csv",sep=";",index=False,mode="w+")
+
+# %% triangle 10 000 points within
+
+# triangle with 10 000 points
+random.seed()
+
+pt1 = (50, 50)
+pt2 = (1000, 80)
+pt3 = (300, 770)
+points = [points_in_triangle(pt1, pt2, pt3) for _ in range(9997)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0]],
+                    "y":[pt1[1], pt2[1], pt3[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("t10000.csv",sep=";",index=False,mode="w+")
+
+# %% triangle 100 000 points within
+
+# triangle with 100 000 points
+random.seed()
+
+pt1 = (50, 50)
+pt2 = (1000, 80)
+pt3 = (300, 770)
+points = [points_in_triangle(pt1, pt2, pt3) for _ in range(99997)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0]],
+                    "y":[pt1[1], pt2[1], pt3[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("t100000.csv",sep=";",index=False,mode="w+")
+
+
+# %% rectangle with 100 points on it (near best case)
+
+# rectangle with 100 points on rectangle
+random.seed()
+
+pt1 = (50, 50)
+pt3 = (1000, 770)
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+points = [points_on_rectangle(pt1, pt2, pt3, pt4) for _ in range(96)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0], pt4[0]],
+                    "y":[pt1[1], pt2[1], pt3[1], pt4[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("r100.csv",sep=";",index=False,mode="w+")
+
+# %% rectangle with 10 000 points on it (near best case)
+
+# rectangle with 10 000 points on rectangle
+random.seed()
+
+pt1 = (50, 50)
+pt3 = (1000, 770)
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+
+points = [points_on_rectangle(pt1, pt2, pt3, pt4) for _ in range(9996)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0], pt4[0]],
+                    "y":[pt1[1], pt2[1], pt3[1], pt4[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("r10000.csv",sep=";",index=False,mode="w+")
+
+# %% rectangle with 100 000 points on it (near best case)
+
+# rectangle with 100 000 points on rectangle
+random.seed()
+
+pt1 = (50, 50)
+pt3 = (1000, 770)
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+
+points = [points_on_rectangle(pt1, pt2, pt3, pt4) for _ in range(99996)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":[pt1[0], pt2[0], pt3[0], pt4[0]],
+                    "y":[pt1[1], pt2[1], pt3[1], pt4[1]]})
+df2 = pd.DataFrame({"x":x, "y":y})
+df = pd.concat([df,df2],ignore_index=True)
+df.to_csv("r100000.csv",sep=";",index=False,mode="w+")
+
+# %% polygone with 100 points within
+
+# polygone with 100 points within it
+random.seed()
+
+pt1 = (10, 10) # limits of coordinates
+pt3 = (1000, 790) # limits of coordinates
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+points = [points_within_polygone(pt1, pt2, pt3, pt4) for _ in range(100)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":x, "y":y})
+df.to_csv("p100.csv",sep=";",index=False,mode="w+")
+
+# %% polygone with 10 000 points within
+
+# polygone with 10 000 points within it
+random.seed()
+
+pt1 = (50, 50)
+pt3 = (1000, 770)
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+
+points = [points_within_polygone(pt1, pt2, pt3, pt4) for _ in range(10000)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":x, "y":y})
+df.to_csv("p10000.csv",sep=";",index=False,mode="w+")
+
+# %% polygone with 100 000 points within
+
+# polygone with 100 000 points within it
+random.seed()
+
+pt1 = (50, 50)
+pt3 = (1000, 770)
+
+pt2 = (pt3[0], pt1[1])
+pt4 = (pt1[0], pt3[1])
+
+
+points = [points_within_polygone(pt1, pt2, pt3, pt4) for _ in range(10000)]
+
+x, y = zip(*points)
+plt.scatter(x, y, s=0.1)
+plt.show()
+
+df = pd.DataFrame({"x":x, "y":y})
+df.to_csv("p10000.csv",sep=";",index=False,mode="w+")
--- jarvis.py
+++ jarvis.py
@@ -28,10 +28,30 @@ def direction(a, b, c):
     # a = hull_point
     # b = candidate_point
     # c = current_point
-
+    
+    # calc cross product
+    cross_product = (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+    len_v1 = ((b[0] - a[0])**2 + (b[1] - a[1])**2)**0.5 # length of vector ab
+    len_v2 = ((c[0] - a[0])**2 + (c[1] - a[1])**2)**0.5 # length of vector ac
+    len_v3 = ((c[0] - b[0])**2 + (c[1] - b[1])**2)**0.5 # length of vector bc
+    
+    #print(cross_product)
     # if cross product = positive the point is clockwise
-
-    return (((b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])) > 0)
+    
+    
+    if cross_product < 0: # current_point is the new candidate_point
+        #print ("hull:", a, "cand:", b, "curr:", c)
+        return True
+    elif ((cross_product == 0) and (len_v3 < 0)):
+        #print ("hull:", a, "cand:", b, "curr:", c)
+    #elif cross_product == 0 and ((((b[0] - a[0])**2 + (b[1] - a[1])**2)**0.5 - (((c[0] - a[0])**2 + (c[1] - a[1])**2)**0.5)) < 0):
+        return True
+    else: # current point is inside the hull, or a following collinear point
+        #print ("hull: ", a, "cand:", b, "curr:", c)
+        return False
+    
+    # return (((b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])) > 0) # return True if positive
+    #((b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])) > 0
 
 
 def showText(string, font, size, color, screen_height, screen_width, screen):
@@ -62,7 +82,7 @@ def quitJarvis():
     pygame.quit
 
 
-def runJarvisAlgo(points_to_create):
+def runJarvisAlgo(points_mode = 0, points_to_create = 1000):
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # General Configuration Settings
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -72,20 +92,18 @@ def runJarvisAlgo(points_to_create):
     screen_height = 800
     screen_offset = 50
     hull = []
-
-    x = np.random.randint(screen_offset, screen_width - screen_offset,
-                          size=points_to_create)
-    y = np.random.randint(screen_offset, screen_height - screen_offset,
-                          size=points_to_create)
-
-    df = pd.DataFrame({"x": x, "y": y})
-
-    # test for rectangle
-    # df = pd.DataFrame({"x": [60,500,60,500], "y": [60,60,500,500]})
-
-    # test speed mode vs graphics mode
-    # df = pd.DataFrame({"x": [60,500,60,500,200,60,80,70,400,800,600,300,200],
-    #                   "y": [60,60,500,500,70,200,600,50,300,500,200,150,620]})
+    
+    if points_mode == 0: # points are generated at random
+        x = np.random.randint(screen_offset, screen_width - screen_offset,
+                              size=points_to_create)
+        y = np.random.randint(screen_offset, screen_height - screen_offset,
+                              size=points_to_create)
+    
+        df = pd.DataFrame({"x": x, "y": y})
+    
+    if points_mode == 1: # points are imported from file
+        df = df_file
+    
 
     data_points = list(map(tuple, df.values))  # make a list
     count_points = len(data_points)
@@ -108,12 +126,11 @@ def runJarvisAlgo(points_to_create):
         candidate_point = data_points[(data_points.index(hull_point) + 1)
                                       % count_points]
         # get the next data point (could be randomly anyone,
-        # we just take the next in the list)
+        # we just take the next point in the datapoint list after the hull_point)
         # hull_point is our last found point in the hull
         # candidate_point is our current candidate for a hull_point
         # current_point is any other point where we check if it is left of
         #     candidate_point
-
         for current_point in data_points:
             if (direction(hull_point, candidate_point, current_point)):
                 candidate_point = current_point
@@ -126,11 +143,11 @@ def runJarvisAlgo(points_to_create):
     # end timer here
 
     df_hull = pd.DataFrame(hull, columns=["x", "y"])
-    df_hull.sort_values(by=["x", "y"], inplace=True)
+    #df_hull.sort_values(by=["x", "y"], inplace=True)
     print(df_hull)
 
 
-def runJarvisGraph(points_to_create):
+def runJarvisGraph(points_mode = 0, points_to_create = 100):
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # General Configuration Settings
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -167,21 +184,20 @@ def runJarvisGraph(points_to_create):
     screen.fill(background_color)
 
     # build dataframe with x and y coordinates
-    x = np.random.randint(screen_offset, screen_width - screen_offset,
-                          size=points_to_create)
-    y = np.random.randint(screen_offset, screen_height - screen_offset,
-                          size=points_to_create)
+    
+    if points_mode == 0: # points are generated at random
+        x = np.random.randint(screen_offset, screen_width - screen_offset,
+                              size=points_to_create)
+        y = np.random.randint(screen_offset, screen_height - screen_offset,
+                              size=points_to_create)
+    
+        df = pd.DataFrame({"x": x, "y": y})
+    
+    if points_mode == 1: # points are imported from file
+        df = df_file
+    
 
-    df = pd.DataFrame({"x": x, "y": y})
-
-    # test rectangle
-    # df = pd.DataFrame({"x": [60,500,60,500],"y": [60,60,500,500]})
-
-    # test speed mode vs graphics mode
-    # df = pd.DataFrame({"x": [60,500,60,500,200,60,80,70,400,800,600,300,200],
-    #                   "y": [60,60,500,500,70,200,600,50,300,500,200,150,620]})
-
-    df["y"] = screen_height - df["y"]  # necessary as y-axis origin in Python is on top left
+    df["y"] = screen_height - df["y"]  # necessary as y-axis origin in pygame is on top left
 
     data_points = list(map(tuple, df.values))  # make a list
     count_points = len(data_points)
@@ -251,6 +267,12 @@ def runJarvisGraph(points_to_create):
                     return False
 
                 elif event.key == pygame.K_r:
+                    pygame.display.quit()
+                    pygame.quit()
+                    # if points_mode == 1:
+                    #     # write data again from df_file in df as y-values
+                    #     # are flipped at the beginning of the graphics mode
+                    #     df = df_file
                     return True
 
     pygame.display.quit()
@@ -282,32 +304,77 @@ def runJarvisGraph(points_to_create):
 # Starter program
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# ++++ General Config Setting ++++
+# ++++ Starting modes ++++
 
 app_mode = 99
 points_to_create = 0
+global points_mode
+points_mode = 99
 
 while not (app_mode == 0 or app_mode == 1):
-    print("\nChose the algorithm mode:\n")
+    print("\nChoose the algorithm mode:\n")
     try:
         app_mode = int(input("0 - speed mode, 1 - graphics mode: "))
     except:
         print("wrong input")
-
-if app_mode == 0:
-    try:
-        points_to_create = int(input("How many data points shall be created (default 100 000) : ") or 100000)
-    except:
-        print("wrong input, proceeding with default")
-        points_to_create = 100000
-    runJarvisAlgo(points_to_create)
-
-elif app_mode == 1:
-    while not (points_to_create >= 3 and points_to_create <= 2000):
+        
+# ++++ Speed mode ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
+if app_mode == 0:                                                           
+    while not (points_mode == 0 or points_mode == 1):
+        print("\nHow to create datapoints?\n")
         try:
-            points_to_create = int(input("How many data points shall be created (max 2000, default 100): ") or 100)
+            points_mode = int(input("\nrandom (0) or import file (1)?: ") or 0)
         except:
             print("wrong input")
-    run = True
-    while run:
-        run = runJarvisGraph(points_to_create)
+            
+    if points_mode == 0: # create random points       
+        try:
+            points_to_create = int(input("How many data points shall be created (default 100 000) : ") or 100000)
+        except:
+            print("wrong input, proceeding with default")
+            points_to_create = 100000
+            
+        runJarvisAlgo(points_mode, points_to_create)
+    
+    if points_mode == 1: # load points from file
+        while True:
+            try:
+                file = input("input filename (must be in same directory): ")
+                df_file = pd.read_csv(file,delimiter=";")
+                break
+            except:
+                print("file not found")
+            
+        runJarvisAlgo(points_mode, 0)
+
+# ++++ Speed mode ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+elif app_mode == 1: # graphics mode
+    while not (points_mode == 0 or points_mode == 1):
+        print("\nHow to create datapoints?\n")
+        try:
+            points_mode = int(input("\nrandom (0) or import file (1)?: ") or 0)
+        except:
+            print("wrong input")
+            
+    if points_mode == 0: # create random points
+        while not (points_to_create >= 3 and points_to_create <= 2000):
+            try:
+                points_to_create = int(input("How many data points shall be created (max 2000, default 100): ") or 100)
+            except:
+                print("wrong input")
+        run = True
+        while run:
+            run = runJarvisGraph(points_mode, points_to_create)
+    
+    if points_mode == 1: # load points from file
+        while True:
+            try:
+                file = input("input filename (must be in same directory): ")
+                df_file = pd.read_csv(file,delimiter=";")
+                break
+            except:
+                print("file not found")
+        run = True
+        while run:
+            run = runJarvisGraph(points_mode, points_to_create)
+    
