456007122e3fd7056d338e33aad9837786c9b298
Thomas Ville <ai22m045@technikum-wien.at>
2022-11-01 13:31:33 +0100

    cleaner gitignore

--- performance_special_cases.py
+++ performance_special_cases.py
@@ -1,10 +1,13 @@
 import pandas
 from timeit import Timer
+import helper
 from divide_conquer import convex_hull
 from jarvis import runJarvisAlgo
 from numpy import genfromtxt
 
-files = ['t100.csv','t10000.csv','t100000.csv','r100.csv','r10000.csv','r100000.csv','p100.csv','p10000.csv','p100000.csv']
+files = ['t100.csv','t10000.csv','t100000.csv','r100.csv','r10000.csv','r100000.csv','p100.csv','p10000.csv','p100000.csv','pCycle.csv']
+
+
 
 for file in files:
     #Extract the xy coordinates
@@ -19,4 +22,5 @@ for file in files:
     #Jarvis March
     average_time = 0
     for i in range(5):
+
         t = Timer(lambda: convex_hull(xy, 0))
